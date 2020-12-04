9f177e4071cbd7a979a803f374f38a72daa0ae90
jsr0904 <72367730+jsr0904@users.noreply.github.com>
2020-12-04 10:45:50 +0900

    Update Inception-based Image Model.py

--- 결점 이미지 분류 분석 프로젝트/codes/image model/Inception-based Image Model.py
+++ 결점 이미지 분류 분석 프로젝트/codes/image model/Inception-based Image Model.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import cv2
 import os
 import glob
@@ -20,10 +17,6 @@ import matplotlib.pyplot as plt
 img_dir = 'C:/cpi_image_test2'
 categories = ['BW_image', 'no_BW_image']
 np_classes = len(categories)
-
-
-# In[15]:
-
 
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 data_aug_gen = ImageDataGenerator(
@@ -43,10 +36,6 @@ img_dir_detail = img_dir + "/" + "BW_image" + "/"
 files = glob.glob(img_dir_detail + '*.png')
 save_to_dir = 'C:/cpi_image_test2/BW_image'
 
-
-# In[ ]:
-
-
 ## DATA Argumentation 
 for i, f in enumerate(files):
     png_name = ''.join(f.split()).split('\\')[1][:-4]
@@ -61,10 +50,6 @@ for i, f in enumerate(files):
         i += 1
         if i > 10:
             break
-
-
-# In[16]:
-
 
 image_w = 256
 image_h = 256
@@ -91,15 +76,8 @@ for idx, BW in enumerate(categories):
         except:
             print(BW, str(i)+" 번째에서 에러 ")
 
-
-# In[ ]:
-
-
 X = np.array(X)
 Y = np.array(y)
-
-
-# In[19]:
 
 
 from sklearn.model_selection import train_test_split
@@ -110,9 +88,6 @@ xy = (X_train, X_test, Y_train, Y_test)
 if os.path.exists('C:/cpi_image_test2/numpy_data_gray') is False:
     os.mkdir('C:/cpi_image_test2/numpy_data_gray')
 np.save("C:/cpi_image_test2/numpy_data/binary_image_data_gray.npy", xy)
-
-
-# In[2]:
 
 
 from tensorflow.keras.models import Sequential
@@ -127,31 +102,11 @@ print(np.bincount(y_train))
 print(np.bincount(y_test))
 
 
-# In[3]:
-
-
 X_train = X_train.reshape(X_train.shape[0], 256, 256, 1).astype('float32') / 255
 X_test = X_test.reshape(X_test.shape[0], 256, 256, 1).astype('float32') / 255
 
-# X_train = X_train.astype('float32') / 255
-# X_test = X_test.astype('float32') / 255
-
-
-# In[4]:
-
-
-X_train.shape
-
-
-# In[5]:
-
-
 import tensorflow as tf
 from tensorflow.keras.layers import Conv2D, MaxPool2D, Concatenate, Flatten, Dense
-
-
-# In[6]:
-
 
 def Model():
     def inception(filters):
@@ -181,18 +136,11 @@ def Model():
     return tf.keras.Model(inputs=x, outputs=y)
 
 
-# In[7]:
-
-
 model = Model()
 model.summary()
 model.compile(optimizer='adam',
               loss='binary_crossentropy',
               metrics=['accuracy'])
-
-
-# In[8]:
-
 
 # checkpoint
 checkpoint_path = 'C:/cpi_image_test2/model/checkpoint_Inception-based model.ckpt'
@@ -221,33 +169,10 @@ callback_list = [
             patience=patient
         )]
 
-
-# In[9]:
-
-
 model.fit(X_train, y_train, batch_size=64, epochs=40, validation_split=0.15,  callbacks= callback_list)
-
-
-# In[10]:
-
 
 model.load_weights(checkpoint_path)
 
-
-# In[11]:
-
-
 print("정확도 : %.2f " %(model.evaluate(X_test, y_test)[1]))
 
-
-# In[12]:
-
-
 model.save("C:/cpi_image_test2/model/Inception-based model.h5")
-
-
-# In[ ]:
-
-
-
-
